580a91141cf4c077fdfc7f1b060a0bd544810092
lucky <lucky329995@gmail.com>
2025-08-10 09:51:08 +0800

    redis

--- src/main/java/com/exchange/controller/RedisAdminController.java
+++ src/main/java/com/exchange/controller/RedisAdminController.java
@@ -1,5 +1,6 @@
 package com.exchange.controller;
 
+import com.exchange.common.RedisKeyConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,17 @@ public class RedisAdminController {
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
+
+    @GetMapping("/snapshot/{symbol}/{side}/{accountType}")
+    public String snapshot(@PathVariable String symbol, @PathVariable String side, @PathVariable String accountType) {
+        String key = RedisKeyConstants.LEAD_POSITION_SNAPSHOT_HASH
+                + ":"
+                + accountType + ":"
+                + symbol + "_"
+                + side;
+        stringRedisTemplate.delete(key);
+        return "Pending close set cleared.";
+    }
 
     @GetMapping("/pending-close")
     public Set<String> listPendingCloseSet() {
