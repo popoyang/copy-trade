daa995bcb9520e062094a25ba47bccaedd4c6f14
benny <>
2025-09-08 20:28:39 +0800

    更新倍数

--- src/main/java/com/exchange/sevice/impl/CopyTradeServiceImpl.java
+++ src/main/java/com/exchange/sevice/impl/CopyTradeServiceImpl.java
@@ -40,7 +40,7 @@ public class CopyTradeServiceImpl implements CopyTradeService {
     @Autowired
     private RedisTemplate<String, String> stringRedisTemplate;
 
-    @Value("${account.ratioMultiplier:2}")
+    @Value("${account.ratioMultiplier:3}")
     private BigDecimal ratioMultiplier;
 
 
